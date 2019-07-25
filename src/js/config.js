94bc885d639fb6b2de0ae343b16b98a60dfce4b9
Kell-Stack <KellySousa1920@gmail.com>
2019-07-25 16:29:23 -0700

    text row iteration for table

--- src/js/config.js
+++ src/js/config.js
@@ -7,80 +7,83 @@ jQuery.noConflict();
   var $cancelButton = $('.js-cancel-button');
   var $message = $('.js-text-message');
   var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-  console.log(config)
 
-  if (config.message) {
+   if (config.message) {
     $message.val(config.message);
   }
-
-  $form.on('submit', function (e) {
+  $form.on('submit', function(e) {
     e.preventDefault();
-    console.log("😎😎😎",{message: $message.val()})
     kintone.plugin.app.setConfig({message: $message.val()}, function() {
       alert('The plug-in settings have been saved. Please update the app!');
       window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
     });
   });
-
   $cancelButton.on('click', function() {
     window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
   });
-
 
   // from create-plugin
 
   //config table 
 
-  //instantiating KEYS for Object "customCell"
-  let customCell = function (){
-    return {
-      init: function ({rowData, updateRowData}) {
-        let spanTag = document.createElement('span');
-        let textField1 = new kintoneUIComponent.Text({value: rowData.text1.value})
-        let textField2 = new kintoneUIComponent.Text({value: rowData.text2.value})
-        
-        spanTag.appendChild(textField1.render());
-        spanTag.appendChild(textField2.render());
-        textField1.on('change', function(newValue){
-          updateRowData({text1:{value:newValue}}, false);
-        })
+  let textRow
 
-        textField2.on('change', function(newValue){
-          updateRowData({text1:{value:newValue}}, false);
 
-        })
+  if (!config.hasOwnProperty('text_row_number')){
+    textRow = Number(config['text_row_field']) //field code
 
-        this.textField1 = textField1
-        this.textField2 = textField2
-        return spanTag
-      },
-      update: function ({rowData}){
-        let text1Val = rowData.text1
-        let text2Val = rowData.text2
 
-        //i'm confused on ._reactObject
-        if (text1Val && this.textField1._reactObject) {
-          this.textField1.setValue(text.text1Val.value)
-        }
-        if (text2Val && this.textField2._reactObject) {
-        }
-      }
+    for (let text = 1; text < textRow + 1; text++) {
+      config['text_row' + text] = JSON.parse(config['text_row' + text])
     }
+  } else {
+    config['body_text'] = JSON.parse(config['body_text'])
+    textRow = 10
   }
 
-  //this data will change and is different from example code
-  // let initialData = [
-  //   {
-  //     text: {value: 'text field'},
-  //     text1: {value: 'text field 1'},
-  //     text3: {value: 'text field 3'},
 
 
+
+  // //instantiating KEYS for Object "customCell"
+  // let customCell = function (){
+  //   return {
+  //     init: function ({rowData, updateRowData}) {
+  //       let spanTag = document.createElement('span');
+  //       let textField1 = new kintoneUIComponent.Text({value: rowData.text1.value})
+  //       let textField2 = new kintoneUIComponent.Text({value: rowData.text2.value})
+        
+  //       spanTag.appendChild(textField1.render());
+  //       spanTag.appendChild(textField2.render());
+  //       textField1.on('change', function(newValue){
+  //         updateRowData({text1:{value:newValue}}, false);
+  //       })
+
+  //       textField2.on('change', function(newValue){
+  //         updateRowData({text1:{value:newValue}}, false);
+
+  //       })
+
+  //       this.textField1 = textField1
+  //       this.textField2 = textField2
+  //       return spanTag
+  //     },
+  //     update: function ({rowData}){
+  //       let text1Val = rowData.text1
+  //       let text2Val = rowData.text2
+
+  //       //i'm confused on ._reactObject
+  //       if (text1Val && this.textField1._reactObject) {
+  //         this.textField1.setValue(text.text1Val.value)
+  //       }
+  //       if (text2Val && this.textField2._reactObject) {
+  //       }
+  //     }
   //   }
-  // ]
+  // }
 
 
-  //this data will change and is different from example code
+  
+
 
 
 
