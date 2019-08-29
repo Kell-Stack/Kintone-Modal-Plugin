488f5cfa1dfef77064cc1a15b4d238aebf3d19bf
Kell-Stack <KellySousa1920@gmail.com>
2019-08-29 14:22:56 -0700

    can now display the icon in the spacer field and show the text entered in the config file

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -5,58 +5,35 @@ import image from '../image/question.png';
 (function (PLUGIN_ID) {
   'use strict';
 
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID)
-
-  const retrieveTextPerElId = (config) => {
-    var items = []
-    config.forEach(index => {
+  const getIcon = () => {
+    var config = kintone.plugin.app.getConfig(PLUGIN_ID)
+    console.log(config,"config 🔴")
+    var parsedConfig = JSON.parse(config.table);
+    parsedConfig.forEach(index => {
       var elementId = index.dropDown.value
       var text = index.text.value
-      var getIcon = (elementId, text) => {
 
-        // console.log(retrieveTextPerElId(parsedConfig), "ysrsrsyrs")
-        // retrieveTextPerElId(parsedConfig).forEach(row => {
-        //   var elementId = row.elementId
-        // })
-    
-        var icon = document.createElement('img')
-        icon.setAttribute('src', image)
-    
-        var spacer = kintone.app.record.getSpaceElement(elementId)
-    
-        console.log(spacer, "🙃")
-    
-    
-        $(icon).attr('class', 'info-icon')
-        spacer.appendChild(icon)
-    
-        var tippyAttr = {
-          placement: 'top',
-          animation: 'fade',
-          theme: 'light-border',
-          inertia: 'true',
-          animation: 'scale',
-          content: text,
-        }
-        tippy(spacer, tippyAttr)
+      var icon = document.createElement('img')
+      icon.setAttribute('src', image)
+
+      var spacer = kintone.app.record.getSpaceElement(elementId)
+
+      console.log(spacer, "🙃")
+
+      $(icon).attr('class', 'info-icon')
+      spacer.appendChild(icon)
+
+      var tippyAttr = {
+        placement: 'top',
+        animation: 'fade',
+        theme: 'light-border',
+        inertia: 'true',
+        animation: 'scale',
+        content: text,
       }
-      
-      // var obj = {}
-      // obj.elementId = elementId
-      // obj.text = text
-      // items.push(obj)
+      tippy(spacer, tippyAttr)
     })
-    // console.log(items, "Ⓜ️")
-    // return items
   }
-
-  // if (config && config.table) {
-  //   var parsedConfig = JSON.parse(config.table)
-  //   console.log(parsedConfig, "🐵")
-  //   retrieveTextPerElId(parsedConfig)
-  // }
-
-
 
   kintone.events.on('app.record.detail.show', function (event) {
     console.log(event, "event:")
